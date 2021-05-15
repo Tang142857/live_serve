d18cd78625fa3b79d2b0064b55ea6148c3d13334
Tang142857 <dfsa00001@qq.com>
2021-05-15 14:20:16 +0800

    improve log function

--- serve_pack/serve.py
+++ serve_pack/serve.py
@@ -7,6 +7,7 @@ The serve serve model.
 @date: 2021-05-03
 Copyright(c): DFSA Software Develop Center
 """
+import os
 import sys
 from http import server
 
@@ -14,7 +15,22 @@ import processor
 
 
 class MainResponse(server.BaseHTTPRequestHandler):
-    def log_in_file(self):
+    def log_request(self, code='-', size='-') -> None:
+        """Complete override the log function to pretty print"""
+        status = 'At %(port)s -> %(visitor)s [%(time)s] - %(line)s - %(scode)s %(size)s/b'
+        info_dict = {
+            'port': self.server.server_address,
+            'time': self.log_date_time_string(),
+            'visitor': str(self.client_address),
+            'line': self.requestline,
+            'scode': code,
+            'size': size
+        }
+        status %= info_dict
+        print(status)
+        self.log_in_file(status)
+
+    def log_in_file(self, info_str: str):
         """
         Called by log requests 
         write the log into file ,closed in --debug
@@ -22,12 +38,15 @@ class MainResponse(server.BaseHTTPRequestHandler):
         if '--debug' in sys.argv:
             return
         else:
-            # TODO log in file
-            pass
+            default_log_path = '%s/.service/log/live_serve.log'
+            default_log_path %= os.path.expanduser('~')
+            try:
+                with open(default_log_path, 'a') as f:
+                    f.write(info_str + '\n')
+            except Exception as ans:
+                print(f'Failed to write log file {str(ans)}')
 
     def do_GET(self):
-        status = f'At port -> {self.server.server_port}'
-        print(status, end='')
         requests_manager(self)
 
 
